194a1f97787e5d575a90bf691075866316d8aff3
Mamadou <bhoye500@gmail.com>
2023-07-13 12:20:06 -0400

    terraform

---
+++ code1/output.tf
@@ -0,0 +1,16 @@
+output "inst_ip" {
+    value = aws_instance.Demo[0].public_ip
+  
+}
+output "inst_pub" {
+    value = aws_instance.Demo[0].private_ip
+  
+}
+output "inst_id" {
+    value = aws_instance.Demo[0].id
+  
+}
+output "public_dns" {
+    value = aws_instance.Demo[0].public_dns
+  
+}
---
+++ code3/data.tf
@@ -0,0 +1,3 @@
+data "aws_instance" "remote-ec2" {
+  instance_id = "i-028c5bd1a50693c33"
+}
